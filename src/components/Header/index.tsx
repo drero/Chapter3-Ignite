2695a7c209106e2cb5683e21acba4ffd8e8610cb
Andre <dreroips@gmail.com>
2021-12-06 09:24:55 -0300

    ok

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,16 @@
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <a>
+          <img src="/logo.svg" alt="logo" />
+        </a>
+      </Link>
+    </header>
+  );
 }
